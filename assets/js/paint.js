010fd04e34d70a7c2b873cd6f57e493b98cc13ad
jwatts41 <wattsjonovan@yahoo.com>
2022-08-16 17:41:33 -0400

    added in switches for colors

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -38,33 +38,57 @@ function getProductInfo(paintColor) {
     
     switch (paintColor) {
         case 'pn1': 
-        price = '19.99'
+        price = '14.99'
         colorName = 'Lime Green'
         updatePrice(colorName, price)          
             // set variables for price and color name and invoke a function to update the price     
             break;           
         case 'pn2':
+            price = '11.14'
+            colorName = 'Medium Brown'
+            updatePrice(colorName, price)
             // set variables for price and color name and invoke a function to update the price    
             break;            
         case 'pn3':
+            price = '22.99'
+            colorName = 'Royal Blue'
+            updatePrice(colorName, price)
             // set variables for price and color name and invoke a function to update the price  
             break;   
         case 'pn4':
+            price = '4.99'
+            colorName = 'Solid Black'
+            updatePrice(colorName, price)
             // set variables for price and color name and invoke a function to update the price  
             break;   
         case 'pn5':
+            price = '8.22'
+            colorName = 'Solid Cyan'
+            updatePrice(colorName, price)
             // set variables for price and color name and invoke a function to update the price       
             break;   
         case 'pn6':
+            price = '11.99'
+            colorName = 'Solid Purple'
+            updatePrice(colorName, price)
             // set variables for price and color name and invoke a function to update the price        
             break;   
         case 'pn7':
+            price = '13.42'
+            colorName = 'Solid Red'
+            updatePrice(colorName, price)
             // set variables for price and color name and invoke a function to update the price 
             break;   
         case 'pn8':
+            price = '21.98'
+            colorName = 'Solid White'
+            updatePrice(colorName, price)
             // set variables for price and color name and invoke a function to update the price   
             break;   
         case 'pn9':
+            price = '14.99'
+            colorName = 'Solid Yellow'
+            updatePrice(colorName, price)
             // set variables for price and color name and invoke a function to update the price 
             break;   
           default:              
@@ -72,10 +96,12 @@ function getProductInfo(paintColor) {
 
     function updatePrice(colorName, price)
     {       
-        let colorPrice = // select element with corresponding id
+        let colorPrice = document.getElementById('color-price');
+        colorPrice.textContent = price; // select element with corresponding id
         // display price
         
-        let color = // select element with corresponding id
+        let color = document.getElementById('color-name');
+        color.textContent = colorName; // select element with corresponding id
         //display color name
     }
     
